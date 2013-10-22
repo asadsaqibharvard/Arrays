895b8d247f7c2a0e7abb9cc4573eace35af58112
Asad Saqib <asadsaqib@fas.harvard.edu>
2013-10-22 01:23:52 -0400

    form POST final

--- logic.php
+++ logic.php
@@ -1,27 +1,32 @@
 <?php
 
+/*
 echo "<pre>";
 echo print_r($_POST);
 echo "</pre>";
+
+
 
 $contestants['Sam'] = '';
 $contestants['Asad'] = '';
 $contestants['John'] = '';
 $contestants['Lance'] = '';
 
+*/
+
 $winning_number = rand(0,4);
 
-foreach ($contestants as $key => $value) {
+foreach ($_POST as $field_name => $contestant_name) {
 
 	$contestants_random_number = rand(0,4);
 
 	//Winner
 	if ($winning_number == $contestants_random_number) {
-		$contestants[$key] = 'Winner';
+		$contestants[$contestant_name] = 'Winner';
 	}
 	//Loser
 	else{
-		$contestants[$key] = 'Loser';
+		$contestants[$contestant_name] = 'Loser';
 	}
 }
 
